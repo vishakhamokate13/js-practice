2db90dc95a82d34b5d815ec7917c7859ae77d93b
vishakhamokate13 <vishakhamokate2005@gmail.com>
2026-05-21 21:02:36 -0700

    added some info

--- script.js
+++
@@ -1,9 +0,0 @@
-
-alert("Hello World");
-        
-        document.write("This is a document write message");
-
-        console.log("This is a console log message");
-        console.warn("warning: This is a console warning message");
-        console.error("This is a console error message");
-    
